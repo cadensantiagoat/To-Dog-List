fd3524f30ac17d8801bc634c97d28e10d9ab4205
Peijun Zhao <89755404+Ling2251@users.noreply.github.com>
2026-04-15 23:05:44 -0700

    Change todoistAPIKey to empty string(use your own key)
    Updated todoistAPIKey to be empty for user customization.

--- To-Dog-List/To-Dog-List/Secrets.swift
+++ To-Dog-List/To-Dog-List/Secrets.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-//Please chang it to your own key here
-let todoistAPIKey = "f738f7a22d2d8dd8e5d22a977b7b6c7030f7d631"
+//Please change it to your own key here
+let todoistAPIKey = ""
 
